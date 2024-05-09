e973df7294e12298a6b5277aec14e29182c2c141
Lee <lee.mugweru@gmail.com>
2024-05-09 14:51:28 +0300

    React

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
+server.js
+
 const jsonServer = require("json-server");
+const cors = require("cors");
+const path = require("path");
 const server = jsonServer.create();
-const router = jsonServer.router("./db.json");
-const middlewares = jsonServer.defaults({
-  static: "./build",
-});
-const PORT = process.env.PORT || 8000;
+const router = jsonServer.router(path.join(__dirname, "db", "db.json"));
+const middlewares = jsonServer.defaults();
+
+server.use(cors());
+server.use(jsonServer.bodyParser);
 server.use(middlewares);
-server.use(
-  jsonServer.rewriter({
-    "/api/*": "/$1",
-  })
-);
 server.use(router);
+
+const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, () => {
-  console.log("Server is running");
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
 });
